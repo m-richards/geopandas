2ccd10e9868dc513046913b231a5b70d45163e15
Matt Richards <mrichards7@outlook.com.au>
2026-03-23 22:50:24 +1100

    update testing assert index equal

--- geopandas/tests/test_merge.py
+++ geopandas/tests/test_merge.py
@@ -216,10 +216,10 @@ class TestMerging:
         # left metadata is set on the result. This is deliberate for now.
         assert type(result) is GeoDataFrame
         assert result._geometry_column_name is None
-        assert_index_equal(pd.Index([0, 1]), result.columns)
+        assert_index_equal(pd.RangeIndex(2), result.columns, exact=True)
 
         gseries2.name = "foo"
         result2 = pd.concat([gseries2, self.gseries], axis=1)
         assert type(result2) is GeoDataFrame
         assert result._geometry_column_name is None
-        assert_index_equal(pd.Index(["foo", 0]), result2.columns)
+        assert_index_equal(pd.Index(["foo", 0]), result2.columns, exact=True)
